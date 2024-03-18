a56bc2e29bf653a353d9d4d29dfc7cd915f00fa0
wahidtamboli6723 <80415484+wahidtamboli6723@users.noreply.github.com>
2024-03-18 09:23:08 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-//your code here
-it('should have audio element with src attribute', () => {
-  cy.visit('your_page_with_audio.html');
-
-  // Use cy.get() to wait for the audio element to be present
-  cy.get('audio').should('have.attr', 'src');
-});
